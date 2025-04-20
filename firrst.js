b283d122040639b249c8a2acc5e0a15204b59ed0
chethanreddy10 <chethanreddy614@gmail.com>
2025-04-20 19:41:53 +0530

    first commit

---
+++ firrst.js
@@ -0,0 +1,4 @@
+elem1=React.createElement("h1",null,"Hello World");
+elem2=React.createElement("h2",null,"Hello World 2");
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render([elem1,elem2]);//so that both 
